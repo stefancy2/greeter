31a71a934cbb0cdd16b68f19dfb9136e91c37175
S.P. van Liempt <Steve@steve.local>
2018-12-17 20:04:03 +0100

    added greeter and main

---
+++ greeter.js
@@ -0,0 +1,20 @@
+
+
+var Greeter = (function(){
+    console.log('hello');
+})();
+
+module.exports = {
+
+sayHelloInEnglish : function() {
+    return "Hello";
+  },
+  
+sayHelloInSpanish : function() {
+    return "Hola";
+}
+};
+
+
+
+
---
+++ main.js
@@ -0,0 +1,4 @@
+var hello = require ("./greeter.js");
+
+console.log(hello.sayHelloInEnglish());
+console.log(hello.sayHelloInSpanish());
